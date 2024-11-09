259b8f7b20e10fc467fc255e865445354afb9f2a
Arjun Maniyani <arjunrmaniyani@gmail.com>
2024-11-09 12:42:06 +0530

    Hide dashboard until implemented

--- config/dashboard.ts
+++ config/dashboard.ts
@@ -6,7 +6,7 @@ export const sidebarLinks: SidebarNavItem[] = [
   {
     title: "MENU",
     items: [
-      { href: "/dashboard", icon: "dashboard", title: "Dashboard" },
+      // { href: "/dashboard", icon: "dashboard", title: "Dashboard" },
       { href: "/emails", icon: "mail", title: "Emails" },
       { href: "/mail-servers", icon: "server", title: "Mail Servers" },
       { href: "/api-keys", icon: "lock", title: "API Keys" },
